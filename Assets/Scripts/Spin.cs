eda284310316302d18fd705da0cb37fbe802c590
Harry Talbot <harry_talbot@outlook.com>
2018-04-19 22:32:05 +0100

    changed terrain, added animals

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
