e0854cb2eb6d3e5760d8c4babc7d8b31eba49617
Christopher Smith <christopher.smith6@hotmail.co.uk>
2018-04-24 00:46:24 +0100

    Push change to Caught

--- Assets/Scripts/EndingController.cs
+++ Assets/Scripts/EndingController.cs
@@ -6,12 +6,24 @@ using UnityEngine.SceneManagement;
 public class EndingController : MonoBehaviour {
 
     private string[] dialogue;
+    private GameObject dialogueBox;
 
 	// Use this for initialization
 	void Start () {
+        dialogueBox = GameObject.Find("DialogueBox");
+
 		if (SceneManager.GetActiveScene().name.Equals("CaughtEnding")) {
             dialogue = new string[2];
-            dialogue[1] = "After one of your companions was caught, the rest of you were quickly round up. The thieves tossed you";
+            dialogue[0] = "After one of your companions was caught, the rest of you were quickly rounded up. The thieves tossed you out onto the street where you trod solemnly onwards.";
+            dialogue[1] = "Disheartened, one by one your companions go their own way until you find yourself returning back to the farm, accepting your fate as a sold animal.";
+        }
+        else if (SceneManager.GetActiveScene().name.Equals("NoCompanionEnding")) {
+            dialogue = new string[2];
+            dialogue[0] = "You ";
+            dialogue[1] = "";
+        }
+        else if (SceneManager.GetActiveScene().name.Equals("HouseVictoryEnding")) {
+
         }
 	}
 	
